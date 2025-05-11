e6bfbfb8cba01ba9ac9e5186b1b490454927fe43
oh7895 <oh7895@hanyang.ac.kr>
2025-05-12 01:42:47 +0900

    FEATURE: Display ChatList

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
@@ -8,6 +8,14 @@ import SettingsScreen from "../screens/SettingsScreen";
 import ChatScreen from "../screens/ChatScreen";
 import NewChatScreen from "../screens/NewChatScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useDispatch, useSelector } from "react-redux";
+import { getFirebaseApp } from "../utils/firbaseHelper";
+import { child, get, getDatabase, off, onValue, ref } from "firebase/database";
+import { setChatsData } from "../store/chatSlice";
+import { ActivityIndicator, View } from "react-native";
+import colors from "../constants/colors";
+import commonStyles from "../constants/commonStyles";
+import { setStoredUsers } from "../store/userSlice";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -41,7 +49,7 @@ const TabNavigator = () => {
   );
 };
 
-const MainNavigator = () => {
+const StackNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Group>
@@ -74,6 +82,85 @@ const MainNavigator = () => {
       </Stack.Group>
     </Stack.Navigator>
   );
+};
+
+const MainNavigator = (props) => {
+  const dispatch = useDispatch();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const userData = useSelector((state) => state.auth.userData);
+  const storedUser = useSelector((state) => state.users.storedUsers);
+
+  useEffect(() => {
+    console.log("Subscribing to firebase listeners");
+
+    const app = getFirebaseApp();
+    const dbRef = ref(getDatabase(app));
+    const userChatsRef = child(dbRef, `userChats/${userData.userId}`);
+    const refs = [userChatsRef];
+
+    onValue(userChatsRef, (querySnapshot) => {
+      const chatIdData = querySnapshot.val() || {};
+      const chatIds = Object.values(chatIdData);
+
+      const chatsData = {};
+      let chatsFoundCount = 0;
+
+      for (let i = 0; i < chatIds.length; i++) {
+        const chatId = chatIds[i];
+        const chatRef = child(dbRef, `chats/${chatId}`);
+        refs.push(chatRef);
+
+        onValue(chatRef, (chatSnapshot) => {
+          chatsFoundCount++;
+
+          const data = chatSnapshot.val();
+
+          if (data) {
+            data.key = chatSnapshot.key;
+
+            data.users.forEach((userId) => {
+              if (storedUser[userId]) return;
+
+              const userRef = child(dbRef, `users/${userId}`);
+
+              get(userRef).then((userSnapshot) => {
+                const userSnapshotData = userSnapshot.val();
+                dispatch(setStoredUsers({ newUsers: { userSnapshotData } }));
+              });
+
+              refs.push(userRef);
+            });
+
+            chatsData[chatSnapshot.key] = data;
+          }
+
+          if (chatsFoundCount >= chatIds.length) {
+            dispatch(setChatsData({ chatsData }));
+            setIsLoading(false);
+          }
+        });
+
+        if (chatsFoundCount == 0) {
+          setIsLoading(false);
+        }
+      }
+    });
+
+    return () => {
+      console.log("Unsubscribing firebase listeners");
+      refs.forEach((ref) => off(ref));
+    };
+  }, []);
+
+  if (isLoading) {
+    <View style={commonStyles.center}>
+      <ActivityIndicator size={"large"} color={colors.primary} />
+    </View>;
+  }
+
+  return <StackNavigator />;
 };
 
 export default MainNavigator;
--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,12 +1,29 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Pressable,
+  FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import colors from "../constants/colors";
 import { useSelector } from "react-redux";
+import DataItem from "../components/DataItem";
+import PageContainer from "../components/PageContainer";
+import PageTitle from "../components/PageTitle";
 
 const ChatListScreen = (props) => {
   const selectedUser = props.route?.params?.selectedUserId;
   const userData = useSelector((state) => state.auth.userData);
+  const storedUsers = useSelector((state) => state.users.storedUsers);
+  const userChats = useSelector((state) => {
+    const chatsData = state.chats.chatsData;
+    return Object.values(chatsData).sort((a, b) => {
+      return new Date(b.updatedAt) - new Date(a.updatedAt);
+    });
+  });
 
   useEffect(() => {
     props.navigation.setOptions({
@@ -36,13 +53,38 @@ const ChatListScreen = (props) => {
   }, [props.route?.params]);
 
   return (
-    <View style={styles.container}>
-      <Text>Chat list screen</Text>
-      <Button
-        title="Go to chat screen"
-        onPress={() => props.navigation.navigate("ChatScreen")}
+    <PageContainer>
+      <PageTitle text="Chats" />
+      <FlatList
+        data={userChats}
+        renderItem={(itemData) => {
+          const chatData = itemData.item;
+          const chatId = chatData.key;
+
+          const otherUserId = chatData.users.find(
+            (uid) => uid !== userData.userId
+          );
+          const otherUser = storedUsers[otherUserId];
+
+          if (!otherUser) return;
+
+          const title = `${otherUser.firstName} ${otherUser.lastName}`;
+          const subTitle = "This will be a message...";
+          const image = otherUser.profilePicture;
+
+          return (
+            <DataItem
+              title={title}
+              subTitle={subTitle}
+              image={image}
+              onPress={() =>
+                props.navigation.navigate("ChatScreen", { chatId })
+              }
+            />
+          );
+        }}
       />
-    </View>
+    </PageContainer>
   );
 };
 
